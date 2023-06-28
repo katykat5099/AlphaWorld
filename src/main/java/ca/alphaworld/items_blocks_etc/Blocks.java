3aa0f2bae5df4df0e2650335a43682c186b4f699
Kaitlyn <katykat5099@gmail.com>
2023-06-27 20:13:13 -0500

    Assets and a Block
    + Rendering for every item has been added
    + Textures for about half the items
    + 1 block

--- src/main/java/ca/alphaworld/items_blocks_etc/Blocks.java
+++ src/main/java/ca/alphaworld/items_blocks_etc/Blocks.java
@@ -1,16 +1,20 @@
 package ca.alphaworld.items_blocks_etc;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
+import net.minecraft.item.BlockItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class Blocks {
-    public static final Block GRASS_ITEM_TILE = new Block(FabricBlockSettings.create());
+    public static final Block GRASS_ITEM_TILE = new Block(FabricBlockSettings.create().breakInstantly().noCollision().mapColor(MapColor.GREEN).strength(1.0f));
 
     //Block Registry
     public static void blockRegistry(){
         Registry.register(Registries.BLOCK, new Identifier("alphaworld", "grass_item_tile"), GRASS_ITEM_TILE);
+        Registry.register(Registries.ITEM, new Identifier("alphaworld", "grass_item_tile"), new BlockItem(GRASS_ITEM_TILE, new FabricItemSettings()));
     }
 }
